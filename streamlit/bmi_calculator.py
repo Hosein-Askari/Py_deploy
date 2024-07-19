bab478e3abad1caacf78dec0e817bba30cf6ede0
Hosein_Askari <101398814+Hosein-Askari@users.noreply.github.com>
2024-07-19 22:23:12 +0330

    Update bmi_calculator.py

--- streamlit/bmi_calculator.py
+++ streamlit/bmi_calculator.py
@@ -17,20 +17,20 @@ if w != 0 and h != 0:
 
     elif 18.5<=result<25 :
         st.success(result)
-        img=Image.open("bmi_img/2.PNG")
+        img=Image.open("streamlit/bmi_img/2.PNG")
         st.image(img)
 
     elif 25<=result<30 :
         st.warning(result)
-        img=Image.open("bmi_img/3.PNG")
+        img=Image.open("streamlit/bmi_img/3.PNG")
         st.image(img)
 
     elif 30<=result<35 :
         st.warning(result)
-        img=Image.open("bmi_img/4.PNG")
+        img=Image.open("streamlit/bmi_img/4.PNG")
         st.image(img)
 
     elif 35<result<40 or result > 40:
         st.error(result)
-        img=Image.open("bmi_img/5.PNG")
+        img=Image.open("streamlit/bmi_img/5.PNG")
         st.image(img)
